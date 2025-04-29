7260adccdc95238afc5adda57440a826bf00d811
Tulika Roy <tulikaroyofficial@gmail.com>
2025-04-29 23:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import gdown
 import os
 
-# Set background image
+#background image
 st.markdown(
     """
     <style>
@@ -21,7 +21,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Shift content more towards the left
+#shifting left
 st.markdown(
     """
     <style>
@@ -37,7 +37,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Download files from Google Drive if not already present
+#Google Drive
 if not os.path.exists('movie_dict.pkl'):
     url = 'https://drive.google.com/uc?id=1FoZwpaGO28UEiytbr3NWtJlpROynXs_0'
     gdown.download(url, 'movie_dict.pkl', quiet=False)
@@ -46,16 +46,15 @@ if not os.path.exists('similarity.pkl'):
     url = 'https://drive.google.com/uc?id=12ra6C_3AZf2Y_OV6vBtSE_Tfjpz00dHR'
     gdown.download(url, 'similarity.pkl', quiet=False)
 
-# Load the files
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-# Function to fetch poster (placeholder for now)
+#fetch poster
 def fetch_poster(movie_id):
-    return "https://via.placeholder.com/150"  # you can improve later
+    return "https://via.placeholder.com/150"  #maybe later 
 
-# Function to recommend movies
+#recommend func
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
@@ -66,7 +65,7 @@ def recommend(movie):
         recommended_movies.append(movies.iloc[i[0]].title)
     return recommended_movies
 
-# Streamlit UI
+#UI
 st.title('Movie Recommender System')
 
 selected_movie_name = st.selectbox('What movie would you like to watch?', movies['title'].values)
